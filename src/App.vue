137ce700de5702ec5b3e38480482f54608d3ea3d
Jared Hall <halljared1@gmail.com>
2021-11-15 20:42:27 -0500

    switch to tabs, add vuex module library

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,36 @@
 <template>
-  <v-app>
-    <nav-drawer>
-      <!-- -->
-    </nav-drawer>
+	<v-app>
+		<nav-drawer>
+			<!-- -->
+		</nav-drawer>
 
-    <v-app-bar app>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-toolbar-title>Collapsing Bar</v-toolbar-title>
-    </v-app-bar>
+		<v-app-bar app>
+			<v-app-bar-nav-icon></v-app-bar-nav-icon>
+			<v-toolbar-title>Collapsing Bar</v-toolbar-title>
+		</v-app-bar>
 
-    <!-- Sizes your content based upon application components -->
-    <v-main>
-      <!-- Provides the application the proper gutter -->
-      <v-container fluid>
-        <!-- If using vue-router -->
-        <router-view></router-view>
-      </v-container>
-    </v-main>
+		<!-- Sizes your content based upon application components -->
+		<v-main>
+			<!-- Provides the application the proper gutter -->
+			<v-container fluid>
+				<!-- If using vue-router -->
+				<router-view></router-view>
+			</v-container>
+		</v-main>
 
-    <v-footer app>
-      <!-- -->
-    </v-footer>
-  </v-app>
-  <!--
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
-  -->
+		<v-footer app>
+			<!-- -->
+		</v-footer>
+	</v-app>
+	<!--
+	<div id="app">
+		<div id="nav">
+			<router-link to="/">Home</router-link> |
+			<router-link to="/about">About</router-link>
+		</div>
+		<router-view />
+	</div>
+	-->
 </template>
 
 <script lang="ts">
@@ -38,32 +38,32 @@ import { Vue, Component } from "vue-property-decorator";
 import NavDrawer from "./components/NavDrawer.vue";
 
 @Component({
-  components: {
-    NavDrawer,
-  },
+	components: {
+		NavDrawer,
+	},
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+	padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+	a {
+		font-weight: bold;
+		color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+		&.router-link-exact-active {
+			color: #42b983;
+		}
+	}
 }
 </style>
--- src/components/NavDrawer.vue
+++ src/components/NavDrawer.vue
@@ -1,26 +1,26 @@
 <template>
-  <v-navigation-drawer v-model="drawer">
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title class="text-h6"> Application </v-list-item-title>
-        <v-list-item-subtitle> subtext </v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
+	<v-navigation-drawer v-model="drawer">
+		<v-list-item>
+			<v-list-item-content>
+				<v-list-item-title class="text-h6"> Application </v-list-item-title>
+				<v-list-item-subtitle> subtext </v-list-item-subtitle>
+			</v-list-item-content>
+		</v-list-item>
 
-    <v-divider></v-divider>
+		<v-divider></v-divider>
 
-    <v-list dense nav>
-      <v-list-item v-for="item in items" :key="item.title" link>
-        <v-list-item-icon>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-icon>
+		<v-list dense nav>
+			<v-list-item v-for="item in items" :key="item.title" link>
+				<v-list-item-icon>
+					<v-icon>{{ item.icon }}</v-icon>
+				</v-list-item-icon>
 
-        <v-list-item-content>
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
-  </v-navigation-drawer>
+				<v-list-item-content>
+					<v-list-item-title>{{ item.title }}</v-list-item-title>
+				</v-list-item-content>
+			</v-list-item>
+		</v-list>
+	</v-navigation-drawer>
 </template>
 
 <script lang="ts">
@@ -28,9 +28,9 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component
 export default class NavDrawer extends Vue {
-  private items: Array<{ icon: string; title: string }> = [
-    { icon: "mdi-view-dashboard", title: "Browse" },
-  ];
-  private drawer = true;
+	private items: Array<{ icon: string; title: string }> = [
+		{ icon: "mdi-view-dashboard", title: "Browse" },
+	];
+	private drawer = true;
 }
 </script>
